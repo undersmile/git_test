c70a8889af58a7104512a30c94bcb905270692cc
ouwanfeng <190487352@qq.com>
2021-08-04 22:51:43 +0800

    the first commit

---
+++ src/main/java/owf/demo/git_test/one/helloworld.java
@@ -0,0 +1,7 @@
+package owf.demo.git_test.one;
+
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/owf/demo/git_test/GitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package owf.demo.git_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
